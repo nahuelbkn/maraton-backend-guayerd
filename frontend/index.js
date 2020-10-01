526bd39007bfc274f2d430a05f53e92d2eea31f2
Nahuel <nahuel.trucco.bkn@gmail.com>
2020-10-01 18:02:27 -0300

    BorrarCompra: Listo | EditarCompra: Casi listo. Revisar.

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,18 @@
 
+
+// PENDIENTE: Queda pendiente una revisión de todo este código, tiene muchas cosas que mejorar, modularizar, etc.
+
+
+
+
+const URL_SERVER = "http://localhost:3001";
+
 document.addEventListener("DOMContentLoaded", init);
 
 function init()
 {
     // Url servidor
-    const URL_SERVER = "http://localhost:3001";
+    
 
 
 
@@ -23,10 +31,6 @@ function init()
     // Eventos ----------------------------------------------------------------------------------------------------------------
     buttonAgregar.addEventListener("click", agregarHandler);
 
-
-
-
-    // 
 
 
 
@@ -74,7 +78,7 @@ function init()
                         <div class="mini-bloque">
                             <label for="paymentMethod">Método de pago</label> </br>
                             <select name="paymentMethod" id="paymentMethod" class="paymentMethod">
-                                <option disabled selected class="disabled">Seleccione método de pago.</option>
+                                <option disabled selected value="0" class="disabled">Seleccione método de pago.</option>
                                 <option value="Tarjeta de crédito">Tarjeta de crédito</option>
                                 <option value="Efectivo">Efectivo</option>
                                 <option value="Bitcoin">Bitcoin</option>
@@ -88,8 +92,8 @@ function init()
                 </form>
             `;
 
-            divNuevaCompra.classList.remove("agregar");
-            divNuevaCompra.classList.add("agregando");
+                divNuevaCompra.classList.remove("agregar");
+                divNuevaCompra.classList.add("agregando");
             divNuevaCompra.innerHTML = tarjetaFormAgregar;
 
             // Alterno función botón
@@ -114,7 +118,6 @@ function init()
     }
 
 
-
     function submitHandler(event)
     {
         event.preventDefault();
@@ -124,10 +127,9 @@ function init()
         const INPUT_products = document.querySelector('input.products');
         const INPUT_amount = document.querySelector('input.amount');
         const SELECT_paymentMethod = document.querySelector('select.paymentMethod');
-        const divNuevaCompra = document.querySelector('div.nuevaCompra');
         
 
-        if ( INPUT_clientId.value && INPUT_products.value && INPUT_amount.value && SELECT_paymentMethod.value )
+        if ( INPUT_clientId.value && INPUT_products.value && INPUT_amount.value && SELECT_paymentMethod.value != 0 )
         {
             let productos = INPUT_products.value.split(",");
             // Se podrían recibir datos de dos maneras diferentes:
@@ -158,6 +160,11 @@ function init()
             busquedaPorID(INPUT_busqueda.value);
         }
     }
+
+
+
+
+
 
 
 
@@ -205,6 +212,7 @@ function init()
                 const bodyTabla = document.querySelector('tbody.compras');
                 let tarjetas = "";
 
+                
                 arrayDatos.forEach(function(compra)
                 {
                     let tarjeta = 
@@ -217,8 +225,8 @@ function init()
                             <td>${compra.paymentMethod}</td>
                             <td>
                                 <div class"botones-BM">
-                                    <button class="put">Editar</button>
-                                    <button class="delete">Borrar</button>
+                                    <button class="put" onclick="editarCompra('${compra.id}')">Editar</button>
+                                    <button class="delete" onclick="borrarCompra('${compra.id}')">Borrar</button>
                                 </div>
                             </td>
                         </tr>
@@ -271,97 +279,263 @@ function init()
             buttonAgregar.textContent = "Agregar compra";
         });
     }
+}
 
-    function busquedaPorID(id) // GET/compras/ID
+
+// Funciones ONCLICK -------------------------------------------------------------------------------------------
+function editarCompra(idEditar)
+{
+    const divEDIT = document.querySelector('div.edit');
+
+    divEDIT.classList.add("editando");
+
+    busquedaPorID(idEditar)
+
+    divEDIT.innerHTML = 
+    `
+        <h3>
+            Editar compra (ID: ${idEditar})
+        </h3>
+        
+        <div class="bloque-datos">
+            <div class="mini-bloque">
+                <label for="clientId">Código cliente</label> </br>
+                <input type="number" name="clientId" id="clientId" class="clientId" placeholder="Ingrese el código del cliente.">
+            </div>
+
+            </br>
+        
+            <div class="mini-bloque">
+                <label for="products">Productos</label> </br>
+                <input type="text" name="products" id="products" class="products" placeholder="Ingrese el/los códigos."></br>
+                <label class="reducir-fuente" for="products">Si es más de un código, </br>ingreselos separados por comas.</label> </br>
+            </div>
+
+            </br>
+
+            <div class="mini-bloque">
+                <label for="amount">Monto de la compra</label> </br>
+                <input type="number" name="amount" id="amount" class="amount" placeholder="Ingrese el monto de la compra.">
+            </div>
+
+            </br>
+
+            <div class="mini-bloque">
+                <label for="paymentMethod">Método de pago</label> </br>
+                <select name="paymentMethod" id="paymentMethod" class="paymentMethod">
+                    <option disabled selected class="disabled">Seleccione método de pago.</option>
+                    <option value="Tarjeta de crédito">Tarjeta de crédito</option>
+                    <option value="Efectivo">Efectivo</option>
+                    <option value="Bitcoin">Bitcoin</option>
+                </select>
+            </div>
+
+            </br>
+        </div>
+
+        <div class="boton">
+            <button class="cancelarCambios" id="cancelarCambios">Cancelar cambios</button>
+            <button type="submit" class="submitCambios" id="submitCambios">Guardar cambios</button>
+        </div>
+    `;
+
+    const buttonCancelEdit = document.querySelector('button.cancelarCambios');
+    const buttonSaveEdit = document.querySelector('button.submitCambios');
+
+    buttonCancelEdit.addEventListener("click", cancelEditHandler)
+    buttonSaveEdit.addEventListener("click", saveEditHandler(idEditar, event))
+    
+
+    function cancelEditHandler(event)
     {
-        fetch(`${URL_SERVER}/compras/${id}`)
-        .then(function(response)
+        event.preventDefault();
+
+        const divEditando = document.querySelector('div.editando');
+
+        divEditando.innerHTML = 
+        `
+            <h3>
+                Cambios cancelados.
+            </h3>
+        `;
+    }
+
+
+    function saveEditHandler(idEdit, event)
+    {
+        event.preventDefault();
+
+        const INPUT_clientId = document.querySelector('input.clientId');
+        const INPUT_products = document.querySelector('input.products');
+        const INPUT_amount = document.querySelector('input.amount');
+        const SELECT_paymentMethod = document.querySelector('select.paymentMethod');
+        
+
+        if ( INPUT_clientId.value && INPUT_products.value && INPUT_amount.value && SELECT_paymentMethod.value != 0 )
+        {
+            let productos = INPUT_products.value.split(",");
+            // Se podrían recibir datos de dos maneras diferentes:
+            //      - "10"
+            //      - "10,20,30,40,50,60,70"
+            // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String/split
+
+            let compraActualizada = 
+            {
+                "clientId": INPUT_clientId.value,
+                "products": productos,
+                "amount": INPUT_amount.value,
+                "paymentMethod": SELECT_paymentMethod.value
+            };
+
+            actualizarDatosEnServidor(idEdit, compraActualizada);
+        }
+    
+    }
+}
+
+function borrarCompra(idBorrar)
+{
+    if( confirm("¿Realmente quiere borrar la compra?") )
+    {
+        fetch(`${URL_SERVER}/compras/${idBorrar}`,{
+            'method':'DELETE'
+        }).then(function(response)
         {
             return response.json();
-        })
-        .then(function(respuesta)
+        }).then(function(respuesta)
         {
-            mostrarRespuesta(respuesta);
-        });
-
-
-        let mostrarRespuesta = function(objRespuesta)
-        {
-            const divNuevaCompra = document.querySelector('div.nuevaCompra');
+            console.log(respuesta.mensaje || respuesta.error );
             
-            divNuevaCompra.innerHTML = "";
-            divNuevaCompra.classList.remove("agregando");
-
             const divLista = document.querySelector('div.lista-cargada');
-            let tarjeta = "";
+            let tarjeta = 
+            `
+                <div class="borrada">
+                    <h3>
+                        Compra borrada.
+                    </h3>
 
-            if ( objRespuesta.id )
-            {
-                // Convertir fecha. (Se puede modularizar) -----------------------
-                let fechaOriginal = objRespuesta.createdAt;
-                let fecha = 
-                    fechaOriginal.substr(8,2) + "-" +
-                    fechaOriginal.substr(5,2) + "-" +
-                    fechaOriginal.substr(0,4) + " " +
-                    fechaOriginal.substr(11,5);
-                    // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String/substr
-                //----------------------------------------------------------------
+                    <button class="volver" onclick="window.location.reload()">Volver al listado</button>
+                </div>
+            `;
 
-                const divBusqueda = document.querySelector('div.busqueda');
-                divBusqueda.innerHTML = `<button class="volver" onclick="window.location.reload()">Volver al listado</button>`;
-                
-                tarjeta = 
-                `
-                    <table class="compras">
-                        <thead>
-                            <tr>
-                                <td>ID compra</td>
-                                <td>Código de cliente</td>
-                                <td>Productos</td>
-                                <td>Monto de la compra</td>
-                                <td>Método de pago</td>
-                                <td>Fecha</td>
-                                <td>Opciones</td>
-                            </tr>
-                        </thead>
-                        <tbody class="compras">
-                            <tr>
-                                <td>${objRespuesta.id}</td>
-                                <td>${objRespuesta.clientId}</td>
-                                <td>${objRespuesta.products}</td>
-                                <td>${objRespuesta.amount}</td>
-                                <td>${objRespuesta.paymentMethod}</td>
-                                <td>${fecha}</td>
-                                <td>
-                                    <div class"botones-BM">
-                                        <button class="put">Editar</button>
-                                        <button class="delete">Borrar</button>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                `;
-            }
-            else
-            {
-                tarjeta = 
-                `
-                    <div class="not-found-id">
-                        <h3>
-                            No se encontró el ID.
-                            <button class="volver" onclick="window.location.reload()">Volver al listado</button>
-                        </h3>
-                    </div>
-                `;             
-            }
-
+    
             divLista.innerHTML = tarjeta;
-        }
+
+            const divEDIT = document.querySelector('div.edit');
+            divEDIT.classList.remove("editando");
+            divEDIT.innerHTML = "";
+        });
     }
 }
 
 
 
+////////////////////////////////////////////////////// Más funciones ////////////////////////////////////////////7
+
+function busquedaPorID(id)
+{
+    fetch(`${URL_SERVER}/compras/${id}`)
+    .then(function(response)
+    {
+        return response.json();
+    })
+    .then(function(respuesta)
+    {
+        mostrarRespuesta(respuesta);
+    });
 
 
+    let mostrarRespuesta = function(objRespuesta)
+    {
+        const divNuevaCompra = document.querySelector('div.nuevaCompra');
+        
+        divNuevaCompra.innerHTML = "";
+        divNuevaCompra.classList.remove("agregando");
+
+        const divLista = document.querySelector('div.lista-cargada');
+        let tarjeta = "";
+
+        if ( objRespuesta.id )
+        {
+            // Convertir fecha. (Se puede modularizar) -----------------------
+            let fechaOriginal = objRespuesta.createdAt;
+            let fecha = 
+                fechaOriginal.substr(8,2) + "-" +
+                fechaOriginal.substr(5,2) + "-" +
+                fechaOriginal.substr(0,4) + " " +
+                fechaOriginal.substr(11,5);
+                // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String/substr
+            //----------------------------------------------------------------
+
+            const divBusqueda = document.querySelector('div.busqueda');
+            divBusqueda.innerHTML = `<button class="volver" onclick="window.location.reload()">Volver al listado</button>`;
+
+
+            tarjeta = 
+            `
+                <table class="compras">
+                    <thead>
+                        <tr>
+                            <td>ID compra</td>
+                            <td>Código de cliente</td>
+                            <td>Productos</td>
+                            <td>Monto de la compra</td>
+                            <td>Método de pago</td>
+                            <td>Fecha</td>
+                            <td>Opciones</td>
+                        </tr>
+                    </thead>
+                    <tbody class="compras">
+                        <tr>
+                            <td>${objRespuesta.id}</td>
+                            <td>${objRespuesta.clientId}</td>
+                            <td>${objRespuesta.products}</td>
+                            <td>${objRespuesta.amount}</td>
+                            <td>${objRespuesta.paymentMethod}</td>
+                            <td>${fecha}</td>
+                            <td>
+                                <div class"botones-BM">
+                                    <button class="put" onclick="editarCompra('${objRespuesta.id}')">Editar</button>
+                                    <button class="delete" onclick="borrarCompra('${objRespuesta.id}')">Borrar</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            `;
+        }
+        else
+        {
+            tarjeta = 
+            `
+                <div class="not-found-id">
+                    <h3>
+                        No se encontró el ID.
+                        <button class="volver" onclick="window.location.reload()">Volver al listado</button>
+                    </h3>
+                </div>
+            `;             
+        }
+
+        divLista.innerHTML = tarjeta;
+    }
+}
+
+function actualizarDatosEnServidor(idEditar, OBJ_Mensaje)
+{
+    fetch(`${URL_SERVER}/compras/${idEditar}`, {
+        method:'PUT',
+        body: JSON.stringify(OBJ_Mensaje),
+        headers:{'Content-Type':'application/json'} 
+    })
+    .then(function(response)
+    {
+        return response.json()
+    })
+    .then(function(mensaje)
+    {
+        console.log(mensaje);
+        const divEditando = document.querySelector('div.editando');
+        divEditando.innerHTML = `<h3>Cambios guardados.</h3>`;
+    });
+}
